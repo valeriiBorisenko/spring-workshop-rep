ff8a94388fec9667e21ff3eaf95ce53e3ac8df01
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-01-09 10:06:22 +0100

    create StudentDaoListImpl

---
+++ src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
@@ -0,0 +1,36 @@
+package se.lexicon.data_access;
+
+import se.lexicon.models.Student;
+
+import java.util.Collections;
+import java.util.List;
+
+public class StudentDaoListImpl implements StudentDao {
+    List<Student> students;
+
+    @Override
+    public Student save(Student student) {
+        students.add(student);
+        return student;
+    }
+
+    @Override
+    public Student find(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return Collections.unmodifiableList(students);
+    }
+
+    @Override
+    public void delete(Student student) {
+        students.remove(student);
+    }
+}
